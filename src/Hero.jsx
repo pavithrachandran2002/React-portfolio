7d505bb9c3f1fc517d12ecc6205f845b228789d7
pavithrachandran2002 <pavithra.chandran2002@gmail.com>
2024-05-26 20:54:36 +0530

    third commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <section className=' flex  w-screen ' >
-         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black-200 md:text-5xl lg:text-6xl dark:text-white mt-20">I'M PAVITHRA :)</h1>
-        <p className="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 xl:px-48 dark:text-black">"I'M A UI/UX DESIGNER,FRONT END DEVELOPER"
-</p>
-<p className="mb-8 text-lg font-normal text-black-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam ea unde omnis at inventore cumque eos alias deserunt voluptatum maxime dolorum non sit, modi consectetur veniam ut ad quaerat nemo?
-</p>
-        
-<div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">  
-<a href ='/pavithra-canva-resume.pdf' download='Pavithra_resume' >
-<button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-whiterounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-white rounded-md group-hover:bg-opacity-0">
- DOWNLOAD RESUME
-</span>
-</button>
-</a>
-   </div> 
-    </div>  
-    <div className='w-1/2 h-1/2 rounded-full'>
-      <img src= "pavithra1.jpg" />
-      
-    </div>
-</section>
-  )
+    <section className=" flex flex-col-reverse justify-center w-screen ">
+      <div className=" px-4 mx-auto max-w-screen-xl text-center lg:px-12">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black-200 md:text-5xl lg:text-6xl dark:text-white mt-2">
+          I'M PAVITHRA :)
+        </h1>
+        <p className="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 xl:px-48 dark:text-black">
+          "I'M A UI/UX DESIGNER,FRONT END DEVELOPER"
+        </p>
+        <p className="mb-8 text-lg font-normal text-black-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam ea
+          unde omnis at inventore cumque eos alias deserunt voluptatum maxime
+          dolorum non sit, modi consectetur veniam ut ad quaerat nemo?
+        </p>
+
+        <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+          <a href="/pavithra-canva-resume.pdf" download="Pavithra_resume">
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-whiterounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-white rounded-md group-hover:bg-opacity-0">
+                DOWNLOAD RESUME
+              </span>
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center mt-5">
+        <img src="pavi123.jpg" className="w-48 h-48 rounded-full object-fill" />
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Navbars from "./Navbar"
-import Hero from "./Hero"
-import "./Home.css"
-import Skills from "./Skills"
-import Contact from "./Contact"
+import React from "react";
+import Navbars from "./Navbar";
+import Hero from "./Hero";
+import "./Home.css";
+import Skills from "./Skills";
+import Contact from "./Contact";
 
 function Home() {
   return (
-    <div className='full bg-lime-700 '>
-    <div className='sticky top-0 nav-p'>
+    <div className=" bg-lime-700 ">
+      <div className="sticky top-0 nav-p">
         <Navbars></Navbars>
-    </div>
+      </div>
 
-    <section className='screen-p'>
+      <section className="screen-p">
         <Hero> </Hero>
-    </section>
+      </section>
 
-    <div className='skill'>
-        <Skills/>
-    </div>
-    <div className='contact'>
-      <Contact/>
-    </div>
-   
-    </div>
+      <div className="screen-p">
+        <Skills />
+      </div>
 
-  )
+      <div className="screen-p">
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
-export default Home
-
+export default Home;
